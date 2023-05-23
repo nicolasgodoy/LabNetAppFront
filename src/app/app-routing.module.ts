032e5c08dc477fafd7eda2b0d4992ae5b0cc6995
Emiliano Quattrone <emilianoquattrone008@gmail.com>
2023-05-23 12:53:12 -0300

    probando angular material

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EditProfileComponent } from './profile/pages/edit-profile/edit-profile.component';
 
 const routes: Routes = [
   {
     path: 'skill',
     loadChildren: () => import('./skill/skill.module').then(m => m.SkillModule)
+  },
+  {
+    path: 'editProfile',
+    component : EditProfileComponent
   }
 ];
 
