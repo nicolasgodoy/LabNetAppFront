3b66eb099ec75bf30e2ef1951c4bffcd6c7cf40e
Emiliano Quattrone <emilianoquattrone008@gmail.com>
2023-05-24 10:01:51 -0300

    Commit photo y CV avance

--- src/app/profile/pages/edit-profile/edit-profile.component.ts
+++ src/app/profile/pages/edit-profile/edit-profile.component.ts
@@ -1,4 +1,7 @@
+import { CloseScrollStrategy } from '@angular/cdk/overlay';
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+import { async, every } from 'rxjs';
 
 @Component({
   selector: 'app-edit-profile',
@@ -7,9 +10,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditProfileComponent implements OnInit {
 
-  constructor() { }
+  public preview : string;
+  public files : any = [];
+  constructor(private sanitizer : DomSanitizer) { }
 
   ngOnInit(): void {
   }
 
+  captureFile(event: any) : any{
+
+    const archivoCapturado = event.target.files[0];
+    this.extraerBase64(archivoCapturado)
+    .then(( img : any) => {
+
+      this.preview = img.base;
+      console.log(img);
+    })
+    this.files.push(archivoCapturado);
+    console.log();
+  }
+
+  extraerBase64 = async ($event: any) => new Promise((resolve, reject) => {
+
+    try {
+      
+      const unsafeImg = window.URL.createObjectURL($event);
+      const img = this.sanitizer.bypassSecurityTrustUrl(unsafeImg);
+      const reader = new FileReader();
+      reader.readAsDataURL($event);
+      reader.onload = () => {
+        resolve({
+          base: reader.result
+        });
+      };
+      reader.onerror = error => {
+        resolve({
+
+          base: null
+        })
+      }
+    } catch (error) {
+      
+      return null;
+    }
+  })
 }
