42e64bdc25223d781f3af13f4a7efe9aeafec1d0
mrkike3 <diegoperez_96@hotmail.com>
2023-05-29 15:21:42 -0300

    mitad Login

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,11 +1,11 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { User } from '../models/user';
 import { map } from 'rxjs/operators';
-import { ResponseDto } from '../models/response';
 import { Observable } from "rxjs";
 import { Login } from "../models/login";
 import { Token } from "../models/token";
+import { ResponseDto } from '../models/response';
+
 
 
 @Injectable({
@@ -15,14 +15,62 @@ import { Token } from "../models/token";
 export class AuthService {
 
     constructor(private http: HttpClient) {
+        this.readToken()
     }
     url:string = "https://localhost:7059/api/login";
-
-    login(login:Login): Observable<Login>{
-        return this.http.post<Login>(this.url + '/Login',login);
-    }
+    userToken: string = '';
+ 
 
 
 
+    login(user: Login) {
+        return this.http.post<ResponseDto>(`${this.url}/login`, user)
+          .pipe(
+            map(resp => {
+              this.saveToken(resp["result"].token, resp["result"].expiration);
+              return resp;
+            })
+          );
+      }
+
+
+
+      private saveToken(idToken: string, expiresIn: number) {
+        this.userToken = idToken;
+        localStorage.setItem('token', idToken);
+    
+        let today = new Date();
+        today.setSeconds(expiresIn);
+        localStorage.setItem('expiresIn', today.getTime().toString());
+      }
+    
+
+
+
+      readToken() {
+        if (localStorage.getItem('token')) {
+          this.userToken = localStorage.getItem('token') || '';
+        } else {
+          this.userToken = '';
+        }
+        return this.userToken;
+      }
+
+      isAuthenticated(): boolean {
+        let response: boolean = false;
+        if (this.userToken.length < 2 ) {
+          response = false;
+        } else {
+          const expiraIn = Number(localStorage.getItem('expiresIn'));
+          const todayExpira = new Date();
+    
+          todayExpira.setTime(expiraIn);
+          if (todayExpira > new Date()) {
+            response = true;
+          }
+        }
+        return response;
+    }   
 }
+
 
