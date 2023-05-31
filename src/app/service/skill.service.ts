8618125ab96f5fe286b62e6ac71c68f3e714e94b
nicolasgodoy <76171813+nicolasgodoy@users.noreply.github.com>
2023-05-31 14:01:00 -0300

    actualizando service de skill

--- src/app/service/skill.service.ts
+++ src/app/service/skill.service.ts
@@ -1,28 +1,40 @@
 import { Injectable } from "@angular/core";
-import {HttpClient} from '@angular/common/http';
+import {HttpClient,HttpHeaders} from '@angular/common/http';
 import { Observable } from "rxjs";
 import { ResponseDto } from "../Response/responseDto";
 import { Skill } from "../models/skill";
 import { AddSkillDto } from "../Response/addSkillDto";
+import { AuthService } from "./auth.service";
+
 
 @Injectable({
     providedIn: 'root'
 })
 export class SkillService {
     
-    constructor(private http: HttpClient){ }
+    constructor(private http: HttpClient,
+        private _authservice:AuthService){ }
 
     url: string = "https://localhost:7059/api/skill";
 
     getSkill(){
-        return this.http.get<ResponseDto>(this.url + "/GetAll");
+        const userToken = `Bearer ${this._authservice.readToken()}`;
+        const headers = new HttpHeaders({ 'Authorization': userToken });
+        const options = { headers: headers };
+        return this.http.get<ResponseDto>(this.url + "/GetAll", options);
     }
 
     AddSkill(addskillDto: AddSkillDto): Observable<AddSkillDto>{
-        return this.http.post<AddSkillDto>(this.url + "/Insert", addskillDto);
+        const userToken = `Bearer ${this._authservice.readToken()}`;
+        const headers = new HttpHeaders({ 'Authorization': userToken });
+        const options = { headers: headers };
+        return this.http.post<AddSkillDto>(this.url + "/Insert", addskillDto,{headers:headers});
     }
 
     deleteSkill(id: number){
-        return this.http.delete<Skill>(this.url + `/Delete/${id}`);
+        const userToken = `Bearer ${this._authservice.readToken()}`;
+        const headers = new HttpHeaders({ 'Authorization': userToken });
+        const options = { headers: headers };
+        return this.http.delete<Skill>(this.url + `/Delete/${id}`, {headers:headers});
     }
 }
