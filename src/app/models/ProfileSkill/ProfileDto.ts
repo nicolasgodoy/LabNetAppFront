5d3c0aad877fcb8c61d1c72fbe6eb048864adcd3
BenjaCardozo <90334282+BenjaCardozo@users.noreply.github.com>
2023-05-31 11:08:30 -0300

    perfil card

--- src/app/models/ProfileSkill/ProfileDto.ts
+++ src/app/models/ProfileSkill/ProfileDto.ts
@@ -1,6 +1,11 @@
+import { Skill } from "../skill";
+
 export interface ProfileDto{
     id?: number;
     name?:string;
     lastName?:string;
     mail?:string;
+    phone?:string;
+    skill: Skill[];
+    //photo:string;
 }
