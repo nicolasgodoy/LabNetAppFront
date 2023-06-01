e5deb15422c628988ee04de69e7ee5f8810caa0f
nicolasgodoy <76171813+nicolasgodoy@users.noreply.github.com>
2023-06-01 14:10:59 -0300

    cambios de estilos

--- src/app/skill/skill-routing.module.ts
+++ src/app/skill/skill-routing.module.ts
@@ -1,15 +1,13 @@
 import { NgModule } from '@angular/core';
 import { ConsultComponent } from './pages/consult/consult.component';
 import { Routes, RouterModule } from '@angular/router';
-import { AddComponent } from './pages/add/add.component';
+
 
 const routes: Routes = [
   {
     path: '',
     children: [
       {path: 'consult', component: ConsultComponent},
-      {path: 'Insert', component: AddComponent}
-      
     ]
   }
 ]
