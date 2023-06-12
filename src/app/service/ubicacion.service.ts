7cc3a2728e288efd5c339d13fdceb9d9ea03f761
Juan Rodriguez <aire19527@gmail.com>
2023-06-12 09:59:57 -0500

    ajuste servicios (token)

--- src/app/service/ubicacion.service.ts
+++ src/app/service/ubicacion.service.ts
@@ -1,24 +1,27 @@
 import { Injectable } from '@angular/core';
-import { Observable } from "rxjs";
+import { Observable } from 'rxjs';
 import { ResponseDto } from '../Response/responseDto';
 import { environment } from 'src/environments/environment';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { AuthService } from './auth.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 
 // https://localhost:7059/api/Ubication/GetAll
-
 export class UbicacionService {
-
   apiUrl: string = environment.apiLab;
   endPoint: string = 'Ubication';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private _authservice: AuthService) {}
 
-  getUbicacion(): Observable<ResponseDto>{
-    let url =  `${this.apiUrl}${this.endPoint}/GetAll`
-    return this.http.get<ResponseDto>(url);
-}
+  getUbicacion(): Observable<ResponseDto> {
+    const userToken = `Bearer ${this._authservice.readToken()}`;
+    const headers = new HttpHeaders({ Authorization: userToken });
+    const options = { headers: headers };
+
+    let url = `${this.apiUrl}${this.endPoint}/GetAll`;
+    return this.http.get<ResponseDto>(url, options);
+  }
 }
